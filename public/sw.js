0a4b0c31f1980f65e8f3d5da91c9d158405ea483
Mayorprimus <mayorprimus@users.noreply.github.com>
2026-09-20 23:35:45 +0100

    Bump service worker cache version (pricing/total-assets/name fixes)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xena-exchange-v1';
+const CACHE_NAME = 'xena-exchange-v2';
 const PRECACHE_URLS = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
